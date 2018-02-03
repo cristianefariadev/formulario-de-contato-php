3756dff430910f809ea0ac7231f0a449218d4658
cristiianefaria <cristiane.trc@gmail.com>
2018-02-03 18:49:23 -0200

    commit inicial

---
+++ index.php
@@ -0,0 +1,83 @@
+<html>
+    <head>
+        <?php include_once 'sendmail.php'; ?>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Código fonte formulario php"/>
+        <meta name="keywords" content="demo, código fonte, formulario php, bootstrap, bootstrap validator, validação"/>
+        <meta name="author" content="Cristiane Faria"/>
+
+        <title>Cristiane Faria | Formulário PHP</title>
+
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
+
+    </head>
+    <body>
+        <div class="container">
+            <h1>Formulário de Contato</h1>
+            <br>
+            <form class="form-horizontal" action="index.php#formulario" method="post" role="form" data-toggle="validator">
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Nome*:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" name="nome" id="nome" value="" placeholder="seu nome" required >
+                        <div class="help-block with-errors"></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Email*:</label>
+                    <div class="col-sm-9">
+                        <input type="email" class="form-control" name="email" id="email" value="" placeholder="exemplo@dominio.com" required>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Telefone*:</label>
+                    <div class="col-sm-9">
+                        <input type="number" class="form-control" name="telefone" id="telefone" placeholder="(00) 00000-0000" required>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Assunto*:</label>
+                    <div class="col-sm-9">
+                        <select class="form-control" name="assunto" required>
+                            <option value="" selected="selected" disabled="disabled"> -- Escolha uma opção --</option>
+                            <option value="contato">Contato</option>
+                            <option value="vendas">Vendas</option>
+                            <option value="financeiro">Financerio</option>
+                            <option value="informacoes">Informações</option>
+                        </select>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Mensagem*:</label>
+                    <div class="col-sm-9">
+                        <textarea class="form-control" id="exampleTextarea" rows="6" 
+                                  id="mensagem" name="mensagem" placeholder="sua mensagem" required></textarea>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-12 text-right">
+                        <input class = "btn btn-primary" id="submit" name="btnSend" type="submit" value="ENVIAR">
+                        <a name="formulario"></a>
+                        <div class="mensagem-alerta"><?php echo $msg ?></div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <footer>
+            <hr>
+            <div class="copyright">Desenvolvido por
+                <a href="http://cristianefaria.com" target="_blank">CRISTIANE FARIA</a>
+            </div>  
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js"></script>
+
+    </body>
+</html>
---
+++ sendmail.php
@@ -0,0 +1,62 @@
+<?php
+
+if (isset($_POST['btnSend'])) {
+
+    $msg = '';
+    
+    //campos do formulario
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $assunto = $_POST["assunto"];
+    $mensagem = $_POST['mensagem'];
+
+    //valida se os campos não estão vazios
+    if ((!empty($nome)) && (!empty($email)) && (!empty($telefone)) && (!empty($assunto)) && (!empty($mensagem))) {
+        
+        $email_remetente = "contato@cristianefaria.com"; // deve ser uma conta de email do seu dominio 
+        $email_destinatario = "contato@cristianefaria.com"; // email que receberá as mensagens
+        $email_recebidoDe = "$email";
+        $email_assunto = "Formulário de Contato"; // Este será o assunto da mensagem
+        $email_conteudo = "FORMULÁRIO DE CONTATO\n"
+            . "<br><b>De:</b> " . $nome
+            . "<br><b>Email:</b> " . $email
+            . "<br><b>Telefone:</b> " . $telefone
+            . "<br><b>Assunto:</b> " . $assunto
+            . "<br><b>Mensagem:</b> " . $mensagem
+            . "<br><br>"
+            . "<hr>"
+            . "<br>Mensagem enviada do formulário de contato da demonstração de formulário de contato com php.";
+
+        //encapsula os dados do cabeçalho do email
+        $email_cabecalho = implode("\n", array("From: $email_remetente", "Reply-To: $email_recebidoDe", "Return-Path: $email_remetente", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
+        
+        //utiliza função nativa do php mail para o envio
+        //valida se o email foi enviado
+        if (mail($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_cabecalho)) {
+            
+            //mostra mensagem de envio com sucesso
+            $msg = '<div class="alert alert-success alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Mensagem enviada com sucesso!</strong> 
+            </div>';
+        } else {
+
+            //mostra mensagem de erro ao enviar
+            $msg = '<div class="alert alert-danger alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Erro ao enviar mensagem, tente novamente! </strong> 
+            </div>';
+        }
+        
+    } else {
+        
+        //mostra mensagem de erro caso algum dos campos esteja vazio
+        $msg = '<div class="alert alert-danger alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Preencha todos os campos!! </strong> 
+            </div>';
+    }
+}
+
+        
